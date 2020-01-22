5f17291d2fc80658712fe6dcd851dc9989234b9b
Fabrício Medeiros <fabricio221995@gmail.com>
2020-01-21 21:17:19 -0300

    Updates

---
+++ precache-manifest.2c9308d5611da189222d50ed190d5ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cabc0003c98c3695b04fea775dab04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589e014b45b329598dfc",
+    "url": "/static/css/main.03d40b26.chunk.css"
+  },
+  {
+    "revision": "5168884686491e251510",
+    "url": "/static/js/2.3f5394da.chunk.js"
+  },
+  {
+    "revision": "6f8c1bc988d0db6bb66a247036a4dbf6",
+    "url": "/static/js/2.3f5394da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "589e014b45b329598dfc",
+    "url": "/static/js/main.2d6115d8.chunk.js"
+  },
+  {
+    "revision": "c96f1ff310eef4803b00",
+    "url": "/static/js/runtime-main.196ea2b9.js"
+  }
+]);
